170575f1e1009c01e7b1c9c90e7f6128ad4f3d83
orefayems <62698264+orefayems@users.noreply.github.com>
2022-10-16 18:30:20 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -2,5 +2,17 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+	numbers = [0]
+	primeNumbers = []
+	if number_of_primes <=0:
+		raise ValueError("cannot have zero or negative number of primes")
+	for i in numbers:
+		if i!=2 and i!=3 and i!=5 and i!=7:
+			if i/1!=1 and i%2 != 0 and i%3 !=0 and i%5 !=0 and i%7 !=0:
+				primeNumbers.append(i)
+		else: 
+			primeNumbers.append(i)
+		if len(primeNumbers) < number_of_primes:
+			i +=1
+			numbers.append(i)
+	return primeNumbers
